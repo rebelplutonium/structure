085edb20ed2ea985308305d7298772b0e5913ee7
Emory Merryman <emory.merryman@gmail.com>
2021-11-22 07:42:07 -0500

    starting

--- flake.nix
+++ flake.nix
@@ -5,7 +5,66 @@
   } ;
   outputs = { self , nixpkgs , flake-utils } : flake-utils.lib.eachDefaultSystem ( system : let pkgs = nixpkgs.legacyPackages.${ system } ; in {
     lib = {
-      dollar = expression : builtins.concatStrings [ "$" "{" ( builtins.toString expression ) "}" ] ;
+      nix-tools = {
+        to-upper-case = builtins.replaceStrings [
+	  "a"
+	  "b"
+	  "c"
+	  "d"
+	  "e"
+	  "f"
+	  "g"
+	  "h"
+	  "i"
+	  "j"
+	  "k"
+	  "l"
+	  "m"
+	  "n"
+	  "o"
+	  "p"
+	  "q"
+	  "r"
+	  "s"
+	  "t"
+	  "u"
+	  "v"
+	  "w"
+	  "x"
+	  "y"
+	  "z"
+	] [
+	  "A"
+	  "B"
+	  "C"
+	  "D"
+	  "E"
+	  "F"
+	  "G"
+	  "H"
+	  "I"
+	  "J"
+	  "K"
+	  "L"
+	  "M"
+	  "N"
+	  "O"
+	  "P"
+	  "Q"
+	  "R"
+	  "S"
+	  "T"
+	  "U"
+	  "V"
+	  "W"
+	  "X"
+	  "Y"
+	  "Z"
+	] ;
+      } ;
+      bash-scripting = {
+        dollar = expression : builtins.concatStringsSep "" [ "$" "{" ( builtins.toString expression ) "}" ] ;
+      } ;
     } ;
   } ) ;
 }
